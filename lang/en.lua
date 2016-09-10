a696c1d484e5a190662347e8c08f2a8f74c2bcae
Scootworks <scootworks@me.com>
2016-09-10 13:05:21 +0200

    update for shadows of the hist
    as i told you, it doesn't work 100%... still when you are in a
    groupfinder party, it still shows role icon, but it shouldn't show it

---
+++ lang/en.lua
@@ -0,0 +1,6 @@
+ZO_CreateStringId("SI_SIUF_POWER_UPDATE" ,			"Power update");
+ZO_CreateStringId("SI_SIUF_LEVEL_UPDATE" ,			"Level update");
+ZO_CreateStringId("SI_SIUF_CP_UPDATE" ,				"CP update");
+ZO_CreateStringId("SI_SIUF_UNIT_CREATED" ,			"Unit created");
+ZO_CreateStringId("SI_SIUF_UNITFRAME_NOT_READY" ,	"Group unit frame was not ready in due time");
+ZO_CreateStringId("SI_SIUF_UNITFRAME_GROUP" ,		"group..");
